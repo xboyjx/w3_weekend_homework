1ada0e86e99d1436ef5bb823b20e05e1fe06640d
Jacob <callaghanjacob@gmail.com>
2022-03-11 15:27:38 +0000

    changes made to render template

--- controllers/controller.py
+++ controllers/controller.py
@@ -7,4 +7,4 @@ from models.example_players import player1, player2
 @app.route('/<player1_choice>/<player2_choice>')
 def winner_page(player1_choice, player2_choice):
     winner = get_winner(player1_choice, player2_choice)
-    return render_template('index.html', title='Home', winner = winner, player1_choice = player1_choice, player2_choice = player2_choice)
+    return render_template('index.html', title='Home', winner = winner)
